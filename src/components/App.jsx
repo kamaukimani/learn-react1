ae6dfdf7bdbef8333bbf062bc15dcc95f391c6e1
kamaukimani <paulkimys285@gmail.com>
2026-05-09 22:03:08 +0300

    start a react learning application

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from './Nav'
+import Home from './Home';
+import About from './About';
+
+function App(){
+    return(
+        <div>
+            Hello World!
+            <NavBar />
+            <Home />
+            <About />
+        </div>
+    )
+}
+export default App;
