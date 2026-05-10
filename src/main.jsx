c2d670419f193d2d361609906974b4818180e5b2
kamaukimani <paulkimys285@gmail.com>
2026-05-10 22:05:44 +0300

    update file

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
-
+import BlogPost from './components/BlogPost'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <BlogPost />
   </StrictMode>,
 )
