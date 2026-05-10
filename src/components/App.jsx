d23c8a197e2c91376696a9b951f20374b86e4112
kamaukimani <paulkimys285@gmail.com>
2026-05-10 22:57:00 +0300

    render ProjectList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import NavBar from './Nav'
 import Home from './Home';
 import About from './About';
 import Links from './Links';
-
+import ProjectList from './ProjectList';
+import user from '../data/user'
 function App(){
     return(
         <div>
@@ -12,6 +13,7 @@ function App(){
             <Home name="Kevin" city="Nyeri" color="blue" />
             <About isBio={true}/>
             <Links github="https://github.com/kamaukimani" linkedIn="https://www.linkedin.com/in/paul-kimani-222a29338/"/>
+            <ProjectList projects={user.projects} />
         </div>
     )
 }
