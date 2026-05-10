b530c3646c775cf55afb306cce3c94ee67f97c3e
kamaukimani <paulkimys285@gmail.com>
2026-05-10 22:03:07 +0300

    update file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
-function Home(){
+import {userName,city} from '../data/user';
+function Home(props){
     return(
         <div id="home">
-            <h1>Home</h1>
+            {/* <h1 style={{
+                color:"purple"
+            }}>{userName} is a web developer from {city}</h1> */}
+            <h1 style={{
+                color:props.color
+            }}>{props.name} is a web developer from {props.city}</h1>
         </div>
     )
 }
