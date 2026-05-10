31181e2f01d3bc0313d3ab7e1558e709ee95f597
kamaukimani <paulkimys285@gmail.com>
2026-05-10 22:15:38 +0300

    add user object

--- src/data/user.js
+++ src/data/user.js
@@ -3,3 +3,35 @@ const city="Nairobi";
 const image="https://i.imgur.com/mV8PQxj.gif";
 
 export { userName,city,image};
+const user = {
+  name: "Paul",
+  city: "Nairobi",
+  bio: "I made this!",
+  color: "firebrick",
+  links: {
+    github: "https://github.com/kamaukimani",
+    linkedin: "https://www.linkedin.com/in/paul-kimani-222a29338/",
+  },
+  projects: [
+    {
+      id: 1,
+      name: "Reciplease",
+      about: "A recipe tracking app",
+      technologies: ["Rails", "Bootstrap CSS"],
+    },
+    {
+      id: 2,
+      name: "Kibbles N Bitz",
+      about: "Tinder for dogs",
+      technologies: ["React", "Redux"],
+    },
+    {
+      id: 3,
+      name: "Alienwares",
+      about: "Etsy for aliens",
+      technologies: ["React", "Redux", "Rails"],
+    },
+  ],
+};
+
+export default user;
