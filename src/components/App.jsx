f4c67f609a87db47ad98b41e166a3668d313ccd2
kamaukimani <paulkimys285@gmail.com>
2026-05-10 22:02:31 +0300

    update file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import NavBar from './Nav'
 import Home from './Home';
 import About from './About';
+import Links from './Links';
 
 function App(){
     return(
         <div>
             Hello World!
             <NavBar />
-            <Home />
-            <About />
+            <Home name="Kevin" city="Nyeri" color="blue" />
+            <About isBio={true}/>
+            <Links github="https://github.com/kamaukimani" linkedIn="https://www.linkedin.com/in/paul-kimani-222a29338/"/>
         </div>
     )
 }
