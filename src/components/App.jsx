36bee8c02bf3f542b2b327522d1d16a528270ec9
kamaukimani <paulkimys285@gmail.com>
2026-05-13 19:42:08 +0300

    render all newly added components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,12 @@ import About from './About';
 import Links from './Links';
 import ProjectList from './ProjectList';
 import user from '../data/user'
+import Header from './Header';
+import About1 from './About1'
+import ArticleList from './ArticleLList';
+import blogData from '../data/blog';
 function App(){
+    console.log(blogData)
     return(
         <div>
             Hello World!
@@ -14,6 +19,9 @@ function App(){
             <About isBio={true}/>
             <Links github="https://github.com/kamaukimani" linkedIn="https://www.linkedin.com/in/paul-kimani-222a29338/"/>
             <ProjectList projects={user.projects} />
+            <Header name={blogData.name}/>
+            <About1 image={blogData.image} about={blogData.about}/>
+            <ArticleList posts={blogData.posts}/>
         </div>
     )
 }
