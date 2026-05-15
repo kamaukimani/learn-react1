17765dd8f8d2b44cd798e7666d2db336448dcb51
kamaukimani <paulkimys285@gmail.com>
2026-05-15 19:58:14 +0300

    add Add to cart and remove from cart logic

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Item({name,category}){
+    const [isClicked,setIsClicked]=useState(false)
+    function handleClick(){
+        setIsClicked(isClicked=>!isClicked)
+    }
+    const line=isClicked? "line-through":null
     return(
         <div>
             <span>Name: {name} </span> 
             <span>Category: {category}  </span>
             {/* <br /> */}
-            <button>Add To Cart</button>
+            <button 
+                style={{textDecoration:line}}
+                onClick={handleClick}>
+                {isClicked ? "Remove From Cart" : "Add To Cart"}
+            </button>
         </div>
     )
 }
