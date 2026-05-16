721aaf9d490d72142bdf41633937eaae3f661f79
kamaukimani <paulkimys285@gmail.com>
2026-05-16 17:06:04 +0300

    render App1.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
 import BlogPost from './components/BlogPost'
+import App1 from './components/App1'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <BlogPost />
+    {/* <App />
+    <BlogPost /> */}
+    <App1 />
   </StrictMode>,
 )
