f8c2ee16aaf2861842079125ad48af2534d9de7b
kamaukimani <paulkimys285@gmail.com>
2026-05-17 14:24:17 +0300

    add props to Form and Display components

--- src/components/App1.jsx
+++ src/components/App1.jsx
@@ -2,9 +2,32 @@ import React,{useState} from 'react'
 import Header2 from './Header2';
 import Profile from './Profile';
 import { UserProvider } from '../context/User';
+import Form from './Form';
+import Display from './Display';
 
 function App1(){
     const [theme,setTheme]=useState('dark')
+    const [firstName,setFirstName]=useState("");
+    const [lastName,setLastName]=useState("");
+    const [newsletter,setNewsletter]=useState(false)
+    const [isValid,setIsValid]=useState(null)
+    function handleFirstName(event){
+        const name=event.target.value
+        setFirstName(name)
+        if(name==="Paul"){
+            setIsValid(null)
+        }else{
+            setIsValid(`${name} is not Paul`)
+        }
+        
+    }
+    function handleLastName(event){
+        setLastName(event.target.value)
+    }
+    function handleCheckbox(event){
+        setNewsletter(event.target.checked)
+    }
+   
     return(
         <div>
             Hello lets learn context
@@ -12,6 +35,16 @@ function App1(){
                 <Header2 theme={theme} setTheme={setTheme}/>
                 <Profile theme={theme}/>
             </UserProvider>
+            <Form firstName={firstName} setFirstName={setFirstName}
+                handleFirstName={handleFirstName}
+                lastName={lastName} setLastName={setLastName} handleLastName={handleLastName}
+                newsletter={newsletter} setNewsletter={setNewsletter} handleCheckbox={handleCheckbox}
+                isValid={isValid}
+            />
+            <Display
+                firstName={firstName}
+                lastName={lastName}
+            />
         </div>
     )
 }
