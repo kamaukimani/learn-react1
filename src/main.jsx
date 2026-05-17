cbd8f7e624f32fa68af3e6c87f90e735dd7dfb7f
kamaukimani <paulkimys285@gmail.com>
2026-05-17 14:32:13 +0300

    render my new App2 component

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './components/App'
 import BlogPost from './components/BlogPost'
 import App1 from './components/App1'
+import App2 from './components1/App'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App />
     <BlogPost /> */}
-    <App1 />
+    {/* <App1 /> */}
+    <App2 />
   </StrictMode>,
 )
