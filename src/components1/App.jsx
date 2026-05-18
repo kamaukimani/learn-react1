ee2fb3a3e36a2ce8dd303494c4a9447f8c2010f2
kamaukimani <paulkimys285@gmail.com>
2026-05-18 20:08:06 +0300

    handle dark mode

--- src/components1/App.jsx
+++ src/components1/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Form from './Form';
+import ShoppingList from './ShoppingList';
+import Header from './Header';
 
 function App2(){
+    const [isDark,setIsDark]=useState(false)
+    function handleDark(){
+        setIsDark(isDark=>!isDark)
+    }
+    const color=isDark ? "black" :"white"
     
     return(
-        <div>
-            Hello, Welcome to my new App
-            <Form/>
+        <div style={{background:color}}>
+            {/* Hello, Welcome to my new App
+            <Form/> */}
+            <Header  isDark={isDark} handleDark={handleDark}/>
+            <ShoppingList />
         </div>
     )
 }
