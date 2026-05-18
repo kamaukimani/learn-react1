0b09909908786b9b91088b1931eed7a360a8481a
kamaukimani <paulkimys285@gmail.com>
2026-05-18 14:40:01 +0300

    remove unnecesary state from App component to Form component

---
+++ src/components1/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from './Form';
+
+function App2(){
+    
+    return(
+        <div>
+            Hello, Welcome to my new App
+            <Form/>
+        </div>
+    )
+}
+export default App2;
